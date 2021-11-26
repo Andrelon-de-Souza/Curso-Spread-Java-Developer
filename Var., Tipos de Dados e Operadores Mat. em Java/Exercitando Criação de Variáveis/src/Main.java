996193ee40c12e11ef73c9cb3a60887373a28b59
Andrelon-de-Souza <dedeh.souza2019@gmail.com>
2021-11-26 17:08:48 -0300

    Inclusão das atividades onde aprendi sobre a criação de variáveis, diferentes tipos de dados e operadores matemáticos no Java

---
+++ Var., Tipos de Dados e Operadores Mat. em Java/Exercitando Criação de Variáveis/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+        final int j = 10;
+        //j = 15;
+        int asrn155253nd;
+        //int afsaf645 47nd;
+        int aregfg55$245_md = 10;
+        //int dfv2$5%2_def = 10;
+
+        asrn155253nd = 100;
+        aregfg55$245_md = 15;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(aregfg55$245_md);
+        System.out.println(aregfg55$245_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
